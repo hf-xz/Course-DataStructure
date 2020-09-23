faab5428e5b68645e4e2aaefdb1f7e1981ff1f93
hfxz_surface <xuan__zai@outlook.com>
2020-09-23 11:52:54 +0800

    1

--- src/cn/edu/bupt/sdmda/ds/linearlist/SeqList.java
+++ src/cn/edu/bupt/sdmda/ds/linearlist/SeqList.java
@@ -1,93 +1,131 @@
 package cn.edu.bupt.sdmda.ds.linearlist;
 
+import java.util.Arrays;
+
 public class SeqList<T> implements LinearList<T>{
 
 	private Object[] _data;
 	private int _size;
 
 	public SeqList(int s, T init){
+		init(s, init);
 	}
 
 	public SeqList(){
+		init(0, null);
 	}
 
 	@Override
 	public void init(int s, T init) {
-		// TODO Auto-generated method stub
-
+		_size = s;
+		_data = new Object[s];
+		for(int i = 0; i < s; i++){
+			_data[i] = init;
+		}
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return true;
+		return (getSize() == 0);
 	}
 
 	@Override
 	public int getSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		return _size;
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
+		_size = 0;
 	}
 
 	@Override
 	public void insert(int i, T t) {
-		// TODO Auto-generated method stub
-		// TODO check i
+		if( !checkWritableRange(i) ) {
+			throw new IndexOutOfBoundsException();
+		}
+		if(_size == _data.length) {
+			Object[] newdata = new Object[_data.length + 1];
+			System.arraycopy(_data, 0, newdata, 0, i);
+			System.arraycopy(_data, i, newdata, i+1, _data.length - i);
+			_data = newdata;
+		}
+		else {
+			System.arraycopy(_data, i, _data, i+1, _size - i);
+		}
+		_size ++;
+		_data[i] = t;
 	}
 
 	@Override
 	public void delete(T t) {
-		// TODO Auto-generated method stub
-
+		int i = find(t);
+		deleteAt(i);
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public T deleteAt(int i) {
-		// TODO Auto-generated method stub
-		return null;
+		if( checkReadableRange(i) ) {
+			throw new IndexOutOfBoundsException();
+		}
+		_size --;
+		T res = (T)_data[i];
+		System.arraycopy(_data, i+1, _data, i, _size-i);
+		return res;
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public T get(int i) {
-		// TODO Auto-generated method stub
-		return null;
+		if(! checkReadableRange(i) ) {
+			throw new IndexOutOfBoundsException();
+		}
+		return (T)_data[i];
 	}
 
 	@Override
 	public void set(int i, T t) {
-		// TODO Auto-generated method stub
-
+		if( !checkReadableRange(i) ) {
+			throw new IndexOutOfBoundsException();
+		}
+		_data[i] = t;
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public int find(T t) {
-		// TODO Auto-generated method stub
-		return 0;
+		for(int i = 0; i < getSize(); i++) {
+			T cur = (T)_data[i];
+			if( cur.equals(t)) return i;
+		}
+		return -1;
 	}
+
+	// private int getMiddile(T[] data, int left, int right) {
+	// 	T mid = data[left];
+	// 	while(left < right) {
+	// 		while(left < right && mid < data[--right]);
+	// 	}
+	// }
+ 	// private void quickSort(T[] data, int start, int end) {
+			
+	// }
 
 	@Override
 	public LinearList<T> sort() {
-		// TODO Auto-generated method stub
+		//211Object[] data = Arrays.copyOf(_data, getSize());
+		
 		return null;
 	}
 
 	private boolean checkReadableRange(int i){
-
-		return true;
-
+		if( i > 0 && i < getSize() -1 ) return true;
+		else return false;
 	}
 
 	private boolean checkWritableRange(int i){
-		
-		return true;
-
+		if ( i > 0 && i < getSize() ) return true;
+		else return false;
 	}
 }
