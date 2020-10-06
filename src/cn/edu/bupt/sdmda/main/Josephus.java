077d66de861e6f821f2f3a5a61b5c7094045f730
hfxz_surface <xuan__zai@outlook.com>
2020-10-06 20:43:03 +0800

    1

--- src/cn/edu/bupt/sdmda/main/Josephus.java
+++ src/cn/edu/bupt/sdmda/main/Josephus.java
@@ -1,5 +1,9 @@
 package cn.edu.bupt.sdmda.main;
 
+import java.util.LinkedList;
+
 public class Josephus {
-    
+    public static void main(String[] args) {
+        System.out.println("约瑟夫环问题求解：请给定总人数n和第k人出局的k（用空格隔开）");
+    }
 }
