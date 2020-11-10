298a8f38a8ee97534a3d5e193635f990e41b466c
hfxz <xuan__zai@outlook.com>
2020-11-10 09:53:31 +0800

    finish HashMap

--- src/cn/edu/bupt/sdmda/ds/hash/MyHash.java
+++ src/cn/edu/bupt/sdmda/ds/hash/MyHash.java
@@ -16,8 +16,8 @@ public class MyHash<K, V> implements IHashTable<K, V> {
 
     public MyHash() {
         // init _table
-
-
+    	_tables = new LinkedList[INIT_CAP];
+    	for(int i = 0; i < INIT_CAP; i++) _tables[i] = new LinkedList<Node>();
         _capacity = INIT_CAP;
         _eleNum = 0;
         _factor = 0.75f;
@@ -32,30 +32,58 @@ public class MyHash<K, V> implements IHashTable<K, V> {
         // if contained, replace it with new value
         // if not, insert it to the linkedlist
         // REMEMBER the element number is increased
-        // REMEMBER to check if resize is necessary
-
+    	// REMEMBER to check if resize is necessary
+    	int idx = getIdx(key);
+    	Node newNode = new Node(key,val);
+    	LinkedList<Node> curList = _tables[idx];
+    	int pos = curList.indexOf(newNode);
+    	if(pos != -1) curList.set(pos, newNode);
+    	else curList.add(newNode);
+    	if(++_eleNum >= _factor*_capacity) resize();
     }
 
     public void resize() {
-
+    	_capacity *= 2;
+    	LinkedList<Node>[] oldTables = _tables;
+    	_tables = new LinkedList[_capacity];
+    	for(LinkedList<Node> curList : oldTables) {
+    		for(Node i : curList)
+    			put(i._key, i._val);
+    	}
     }
 
     @Override
     public V get(K key) {
         // get the value of key
         // if not found, return null
+    	int idx = getIdx(key);
+    	LinkedList<Node> curList = _tables[idx];
+    	for(Node i : curList)
+    		if(i._key == key) return i._val; 
         return null;
     }
 
     @Override
     public V remove(K key) {
+    	--_eleNum;
+    	int idx = getIdx(key);
+    	LinkedList<Node> curList = _tables[idx];
+    	Node tmp = new Node(key,null);
+    	for(Node i : curList)
+			if(i.equals(tmp)) {
+				curList.remove(i);
+				return i._val;
+			}
         return null;
     }
 
     @Override
     public int getIdx(K key) {
         // design a method to get index from key
-        return 0;
+    	int h = key.hashCode();
+    	h = h ^ (h>>>16);
+    	h = h & (_capacity-1);
+        return h;
     }
 
 
@@ -75,7 +103,7 @@ public class MyHash<K, V> implements IHashTable<K, V> {
         // node1.val and node2.val is NOT CONSIDERATE HERE!
         @Override
         public boolean equals(Object obj) {
-            return false;
+            return _key == ((Node)obj)._key;
         }
     }
 }
